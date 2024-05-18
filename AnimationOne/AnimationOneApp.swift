1dd1f38cbb4cdd1a4e926c571a1c1a7119ea253b
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-18 23:09:52 +0500

    Initial Commit

---
+++ AnimationOne/AnimationOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationOneApp.swift
+//  AnimationOne
+//
+//  Created by Abdusamad Mamasoliyev on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
