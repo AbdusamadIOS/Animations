72de8eff6d6c308a45080ae97d1d5f8e3fbab131
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-20 19:07:06 +0500

    animation

--- AnimationOne/ContentView.swift
+++ AnimationOne/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scaleInOut = false
+    @State private var rotateInOut = false
+    @State private var moveInOut = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+            ZStack {
+                
+                // MARK: circle 1
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                
+                // MARK: circle 2
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(60))
+                
+                // MARK: circle 3
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? -60 : 0)
+                    
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color.green, Color.white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: moveInOut ? 60 : 0)
+                }.opacity(0.5)
+                    .rotationEffect(.degrees(120))
+                
+            }.rotationEffect(.degrees(rotateInOut ? 90 : 0 ))
+                .scaleEffect(scaleInOut ? 1 : 1/4)
+                .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8))
+            .onAppear {
+                moveInOut.toggle()
+                scaleInOut.toggle()
+                rotateInOut.toggle()
+            }
         }
-        .padding()
     }
 }
 
